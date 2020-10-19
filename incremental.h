28844a59bb411161c1dc8a158e65007208611a9c
chicodias <chicodias@outlook.com>
2020-10-19 19:36:35 -0300

    incremental e embrulho

--- incremental.h
+++ incremental.h
@@ -1,3 +1,23 @@
+#include "lista.h"
 // file: incremental.h
-// author:  Francisco Rosa Dias de Miranda
+// authors:  Francisco Rosa Dias de Miranda e Hiago
+#ifndef INCREMENTAL_H
+#define INCREMENTAL_H
 
+// definição do tipo de lista utilizada
+
+// algoritmo incremental de cálculo do fecho convexo
+LISTA incremental(char pos, int inic, LISTA * l);
+
+/* Esta primitiva consiste em sabermos a posição de um ponto em
+ relação a um vetor (segmento orientado). Então dados três pontos
+  A, B e C tal que AB é um segmento orientado, temos que a primitiva
+  Esquerda(A,B,C) é verdadeira se o ponto C está a esquerda da reta
+ formada ao estendermos o segmento AB nos dois sentidos. A primitiva 
+ é falsa caso contrário. Analogamente temos a primitiva Direita.
+*/
+boolean esquerda(NO a, NO b, NO c);
+boolean direita(NO a, NO b, NO c);
+
+
+#endif
