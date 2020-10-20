5d5636ce2abcb8cf65c178efc2cc8878b5fdd241
chicodias <chicodias@outlook.com>
2020-10-20 20:47:23 -0300

    embrulho

--- embrulho.c
+++ embrulho.c
@@ -1,5 +1,8 @@
 #include "lista.h"
 #include "embrulho.h"
+// pra usar esse bereguenaits tem que compilar com a tag -lm
+#include <math.h>
+
 
 /* Algoritmo do embrulho.
 Inicialmente, determine o ponto P do conjunto com menor coordenada y.
@@ -15,8 +18,20 @@ NO * findlowest(LISTA * L)
     return L->inicio;
 }
 
-double angulo(NO p, NO q, NO r)
+double angulo(NO * p, NO * q, NO * r)
 {
+    double ang;
+    NO * PQ, * PR;
 
-    return //produto escalar
+    PQ->x = q->x - p->x;
+    PQ->y = q->y - p->y;
+
+    PR->x = r->x - p->x;
+    PR->y = r->y - p->y;
+
+    // cosseno do trem
+    ang = (PQ->x*PR->x + PQ->y*PR->y) /
+          sqrt(PQ->x * PQ->x + PQ->y * PQ->y) * sqrt(PR->x * PR->x + PR->y * PR->y);
+
+    return acos(ang); //produto escalar daquele treco
 }
