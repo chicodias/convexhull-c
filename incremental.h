20fb4ef40fa606c26811bdb972dd45036efdcebe
Hiago <hiagovamerico@usp.br>
2020-10-22 15:37:34 -0300

    Duas funções adicionadas

--- incremental.h
+++ incremental.h
@@ -9,4 +9,10 @@
 // algoritmo incremental de cálculo do fecho convexo
 LISTA incremental(char pos, int inic, LISTA * l);
 
+// função que calcula sentido ant horario
+void anthorario(LISTA *l, NO *p, NO *q, NO *r, int sentido);
+
+// função que calcula sentido horário
+void horario(LISTA *l, NO *p, NO *q, NO *r, int sentido);
+
 #endif
